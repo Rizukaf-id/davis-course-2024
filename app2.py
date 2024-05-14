2672b80b610b04072f56ab91247d185ccbd87a9e
Rizka Fiddiyansyah <68379809+Rizukaf-id@users.noreply.github.com>
2024-05-14 10:26:49 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -10,9 +10,9 @@ st.bar_chart(chart_data)
 
 
 # Initialize connection.
-# conn = st.connection('mysql', type='sql', username=st.secrets["DB_USER"], password=st.secrets["DB_PASS"], host=st.secrets["HOST"], database=st.secrets["DB"])
+conn = st.connection('mysql', type='sql', username='davis2024irwan', password='wh451n9m@ch1n3', host='kubela.id', database='aw')
 # conn = st.connection(**st.secrets.db_credentials)
-conn = st.connection("mydb", type="sql", autocommit=True)
+# conn = st.connection("mydb", type="sql", autocommit=True)
 
 # Perform query.
 df = conn.query('SELECT EnglishPromotionName, StartDate, EndDate, MaxQty from dimpromotion limit 10;', ttl=600)
